a35ccfb7d18141ec2ca8ada3e246ca90ef8b21f1
Taniya-eGov <taniyapushey@gmail.com>
2024-07-09 10:17:06 +0530

    Add unit tests for the MagicalArena class

--- src/main/java/org/magicalarena/MagicalArena.java
+++ src/main/java/org/magicalarena/MagicalArena.java
@@ -1,4 +1,74 @@
 package org.magicalarena;
 
+/**
+ * Represents the magical arena where players fight.
+ */
 public class MagicalArena {
+    private Player playerA;
+    private Player playerB;
+    private Dice dice;
+
+    /**
+     * Constructor to initialize the magical arena with two players.
+     *
+     * @param playerA The first player.
+     * @param playerB The second player.
+     */
+    public MagicalArena(Player playerA, Player playerB) {
+        this.playerA = playerA;
+        this.playerB = playerB;
+        this.dice = new Dice();
+    }
+
+    /**
+     * Simulates a fight between two players until one player's health reaches zero.
+     */
+    public void startFight() {
+        while (playerA.isAlive() && playerB.isAlive()) {
+            if (playerA.getHealth() <= playerB.getHealth()) {
+                attack(playerA, playerB);
+                if (playerB.isAlive()) {
+                    attack(playerB, playerA);
+                }
+            } else {
+                attack(playerB, playerA);
+                if (playerA.isAlive()) {
+                    attack(playerA, playerB);
+                }
+            }
+        }
+    }
+
+    /**
+     * Simulates an attack from the attacking player to the defending player.
+     *
+     * @param attacker The attacking player.
+     * @param defender The defending player.
+     */
+    private void attack(Player attacker, Player defender) {
+        int attackRoll = dice.roll();
+        int defendRoll = dice.roll();
+        int attackDamage = attacker.getAttack() * attackRoll;
+        int defendStrength = defender.getStrength() * defendRoll;
+        int damageDealt = Math.max(0, attackDamage - defendStrength);
+        defender.reduceHealth(damageDealt);
+
+        System.out.println(attacker.getName() + " attacks " + defender.getName() +
+                " with roll " + attackRoll + " (attack damage: " + attackDamage + ") " +
+                "defended by roll " + defendRoll + " (defend strength: " + defendStrength + "). " +
+                defender.getName() + " health is now " + defender.getHealth());
+    }
+
+    /**
+     * Returns the winner of the fight.
+     *
+     * @return The player with health greater than zero.
+     */
+    public Player getWinner() {
+        if (playerA.isAlive()) {
+            return playerA;
+        } else {
+            return playerB;
+        }
+    }
 }
---
+++ src/test/java/org/magicalarena/MagicalArenaTest.java
@@ -0,0 +1,32 @@
+package org.magicalarena;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit tests for the MagicalArena class.
+ */
+public class MagicalArenaTest {
+    @Test
+    public void testFight() {
+        Player playerA = new Player("Player A", 50, 5, 10);
+        Player playerB = new Player("Player B", 100, 10, 5);
+
+        MagicalArena arena = new MagicalArena(playerA, playerB);
+        arena.startFight();
+
+        assertTrue(playerA.getHealth() == 0 || playerB.getHealth() == 0);
+    }
+
+    @Test
+    public void testGetWinner() {
+        Player playerA = new Player("Player 1", 50, 5, 10);
+        Player playerB = new Player("Player 2", 100, 10, 5);
+
+        MagicalArena arena = new MagicalArena(playerA, playerB);
+        arena.startFight();
+
+        Player winner = arena.getWinner();
+        assertTrue(winner.isAlive());
+    }
+}
